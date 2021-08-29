72fe6da61a06389ba552716312638dd0813fe517
yoppi19850616 <azm.y_t_m@icloud.com>
2021-08-30 05:59:36 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'furimas/index'
   devise_for :users
+  root to: "furimas#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
