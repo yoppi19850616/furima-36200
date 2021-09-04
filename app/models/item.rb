e7727c353734c4d2a84a54a25bb2309dadf9f918
yoppi19850616 <azm.y_t_m@icloud.com>
2021-09-04 22:00:10 +0900

    配送についての項目を実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,9 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_charge
+  belongs_to :area
 
   belongs_to :user
   has_one_attached :image
@@ -8,12 +11,11 @@ class Item < ApplicationRecord
   validates :title,              presence: true
   validates :description,        presence: true
   validates :category_id,        presence: true, numericality: {other_than: 1, message:"can't be blank" }
-  validates :status_id,          presence: true
-  validates :shipping_charge_id, presence: true
-  validates :area_id,            presence: true
-  validates :send_date_id,       presence: true
+  validates :status_id,          presence: true, numericality: {other_than: 1, message:"can't be blank" }
+  validates :shipping_charge_id, presence: true, numericality: {other_than: 1, message:"can't be blank" }
+  validates :area_id,            presence: true, numericality: {other_than: 1, message:"can't be blank" }
+  validates :send_date_id,       presence: true, numericality: {other_than: 1, message:"can't be blank" }
   validates :price,              presence: true
   validates :image,              presence: true
-
 
 end
---
+++ app/models/shipping_charge.rb
@@ -0,0 +1,10 @@
+class ShippingCharge < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '着払い' },
+    { id: 3, name: '送料込み' },
+  ]
+  
+  include ActiveHash::Associations
+  has_many :items
+end
