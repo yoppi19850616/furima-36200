f4d9ec1f87f18781bede3bcb0207442b60669d85
yoppi19850616 <azm.y_t_m@icloud.com>
2021-09-11 11:59:52 +0900

    FactoryBotを導入

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '出品登録' do
+    it '登録できる' do
+    end
+  end
+  # pending "add some examples to (or delete) #{__FILE__}"
 end
