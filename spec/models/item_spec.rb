4acd1c2b97df105f2025066b16f10a579e082168
yoppi19850616 <azm.y_t_m@icloud.com>
2021-09-11 14:01:12 +0900

    エラーコードの想定を記入

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,8 +2,34 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   describe '出品登録' do
-    it '登録できる' do
+    context '出品できるとき' do
+      it 'title,description,category_id,status_id,shipping_charge_id,area_id,
+          send_date_id,price,imageが存在すれば登録できる' do
+      end
+    end
+    context '出品できないとき' do
+      it 'titleが空では登録できない' do
+      end
+      it 'descriptionが空では登録できない' do
+      end
+      it 'category_idが---では登録できない' do
+      end
+      it 'status_idが---では登録できない' do
+      end
+      it 'shipping_charge_idが---では登録できない' do
+      end
+      it 'area_idが---では登録できない' do
+      end
+      it 'send_date_idが---では登録できない' do
+      end
+      it 'priceが空では登録できない' do
+      end
+      it 'priceが9,999,999を超えていては登録できない' do
+      end
+      it 'priceが300未満では登録できない' do
+      end
+      it 'imageが空では登録できない' do
+      end
     end
   end
-  # pending "add some examples to (or delete) #{__FILE__}"
 end
