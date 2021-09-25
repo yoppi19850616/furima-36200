805937fd066fce8b34bb00d69ac71830b1137704
yoppi19850616 <azm.y_t_m@icloud.com>
2021-09-25 16:59:08 +0900

    migrateに記述

--- db/migrate/20210925075328_create_addresses.rb
+++ db/migrate/20210925075328_create_addresses.rb
@@ -1,6 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
+      t.string   :postal_code,        null: false
+      t.string   :municipalities,        null: false
+      t.string   :number_address,        null: false
+      t.string   :building_name,        null: false
+      t.string   :phone_number,        null: false
+      t.references :buyer,            null: false, foreign_key:true
+      t.integer :area_id,        null: false
 
       t.timestamps
     end
