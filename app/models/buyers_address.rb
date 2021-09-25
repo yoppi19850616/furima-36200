b09e43cbcf1e0372feaf664c243abdbd3471ce8a
yoppi19850616 <azm.y_t_m@icloud.com>
2021-09-25 17:30:14 +0900

    with optionを設定

--- app/models/buyers_address.rb
+++ app/models/buyers_address.rb
@@ -2,8 +2,13 @@ class BuyersAdress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefectures, :municipalities, :number_address, :building_name, :phone_number
 
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    
+  end
   def save
     buyers = Buyers.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefectures: prefectures, municipalities: municipalities, number_address: number_address, building_name: building_name, phone_number: phone_number)
   end
-  
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_23_064150) do
+ActiveRecord::Schema.define(version: 2021_09_25_075328) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,19 @@ ActiveRecord::Schema.define(version: 2021_09_23_064150) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "postal_code", null: false
+    t.string "municipalities", null: false
+    t.string "number_address", null: false
+    t.string "building_name", null: false
+    t.string "phone_number", null: false
+    t.bigint "buyer_id", null: false
+    t.integer "area_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["buyer_id"], name: "index_addresses_on_buyer_id"
   end
 
   create_table "buyers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -76,6 +89,7 @@ ActiveRecord::Schema.define(version: 2021_09_23_064150) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "addresses", "buyers"
   add_foreign_key "buyers", "items"
   add_foreign_key "buyers", "users"
   add_foreign_key "items", "users"
