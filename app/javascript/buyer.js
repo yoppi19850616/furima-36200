a381c7f9176aca6afc6a3548b7911431049fc13a
yoppi19850616 <azm.y_t_m@icloud.com>
2021-09-29 12:30:52 +0900

    トークン発行を確認,formにトークンを追加

--- app/javascript/buyer.js
+++ app/javascript/buyer.js
@@ -3,7 +3,25 @@ const pay = () => {
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("buyers_address[number]"),
+      cvc: formData.get("buyers_address[cvc]"),
+      exp_month: formData.get("buyers_address[exp_month]"),
+      exp_year: `20${formData.get("buyers_address[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden" >`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+    });
   });
 };
 
