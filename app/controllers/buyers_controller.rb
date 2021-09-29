f7ec04c3fa254b43d66f908aa1f4124f70f70bfd
yoppi19850616 <azm.y_t_m@icloud.com>
2021-09-29 15:57:31 +0900

    createコードを整理

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -8,12 +8,7 @@ class BuyersController < ApplicationController
     @buyers = BuyersAddress.new(buyer_params)
     @item = Item.find(params[:item_id])
     if @buyers.valid?
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
-      Payjp::Charge.create(
-        amount: @item.price,  # 商品の値段
-        card: buyer_params[:token],    # カードトークン
-        currency: 'jpy'                 # 通貨の種類（日本円）
-      )
+      pay_item
        @buyers.save
        redirect_to root_path
     else
@@ -28,4 +23,14 @@ private
                                   .merge(user_id: current_user.id, item_id: params[:item_id],
                                    token: params[:token])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
+      Payjp::Charge.create(
+        amount: @item.price, 
+        card: buyer_params[:token],  
+        currency: 'jpy' 
+      )
+  end
+
 end
