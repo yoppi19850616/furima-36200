ce3f8dcc3d07894e650b6696749cdc9803c7851d
yoppi19850616 <azm.y_t_m@icloud.com>
2021-09-29 21:38:29 +0900

    sold out表記を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :item_find, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit]
+  # before_action :move_to_index_soldout, only: [:edit]
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -50,10 +51,14 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
+    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id 
   end
 
   def item_find
     @item = Item.find(params[:id])
   end
+
+  # def move_to_index_soldout
+  #   redirect_to action: :index item.buyer.nil?
+  # end
 end
