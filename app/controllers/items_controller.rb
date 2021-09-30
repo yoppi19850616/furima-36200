7c6adcffad1903ee06306c0d6c151ce600880222
yoppi19850616 <azm.y_t_m@icloud.com>
2021-09-30 18:25:39 +0900

    直接urlを記述してもページに飛ばないよう実装した

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :item_find, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit]
-  # before_action :move_to_index_soldout, only: [:edit]
+  before_action :move_to_index_soldout, only: [:edit]
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -58,7 +58,7 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  # def move_to_index_soldout
-  #   redirect_to action: :index item.buyer.nil?
-  # end
+  def move_to_index_soldout
+    redirect_to action: :index unless @item.buyer.nil?
+  end
 end
--- spec/factories/buyers_address.rb
+++ spec/factories/buyers_address.rb
@@ -1,10 +1,10 @@
-FactoryBot.define do
-  factory :buyers_address do
-    postal_code    { '123-4567' }
-    area_id        { 2 }
-    municipalities {'大阪市'}
-    number_address { '1-1-1' }
-    building_name  {'テストビル１号館'}
-    phone_number   {'09012345678'}
-  end
-end
+# FactoryBot.define do
+#   factory :buyers_address do
+#     postal_code    { '123-4567' }
+#     area_id        { 2 }
+#     municipalities {'大阪市'}
+#     number_address { '1-1-1' }
+#     building_name  {'テストビル１号館'}
+#     phone_number   {'09012345678'}
+#   end
+# end
